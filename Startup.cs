eb54d0fbb47a30e931bffd89fc429cce57a36cfc
Ashish <ashish.deora@greatplacetowork.com>
2021-04-22 23:55:15 +0530

    Added Cors

--- Startup.cs
+++ Startup.cs
@@ -31,6 +31,7 @@ namespace API
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
             });
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -46,6 +47,8 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
 
             app.UseAuthorization();
 
